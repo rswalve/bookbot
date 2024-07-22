39b4cbdb783e70b8b73fcb14cbbbd3bc9ae30663
rswalve <robert.swalve@gmail.com>
2024-07-22 14:48:52 -0500

    a description of your changes here

---
+++ main.py
@@ -0,0 +1,45 @@
+def main():
+	book_path = "books/frankenstein.txt"
+	text = get_book_text(book_path)
+	num_words = get_num_words(text)
+	letter_count = get_chars_dict(text)
+	print("--- Begin report of books/frankenstein.txt ---")
+	print(f"{num_words} words found in the document")
+	print()
+      
+	list_of_dicts = []
+	for key, value in letter_count.items():
+		new_dict = {"char": key, "num": value}
+		list_of_dicts.append(new_dict)
+            
+	list_of_dicts.sort(reverse=True, key=sort_on)
+	
+	for item in list_of_dicts:
+		print(f"The '{item['char']}' character was found {item['num']} times")
+            
+	print("--- End report ---")
+
+def get_num_words(text):
+	words = text.split()
+	return len(words)
+
+def get_book_text(path):
+	with open(path) as f:
+		return f.read()
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered.isalpha():  # Only consider alphabetic characters
+            if lowered in chars:
+                chars[lowered] += 1
+            else:
+                chars[lowered] = 1
+    return chars
+
+def sort_on(dict):
+      return dict["num"]
+
+if __name__ == "__main__":
+    main()
